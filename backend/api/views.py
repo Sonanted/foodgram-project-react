c22990fd275f7b7c11520f2652b148bb4659e119
Andrey Nikonenko <sonanted@gmail.com>
2022-08-06 16:12:12 +0300

    filters fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 from django.db.models.aggregates import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from djoser.serializers import SetPasswordSerializer
 from rest_framework import status, validators, viewsets
 from rest_framework.decorators import action
